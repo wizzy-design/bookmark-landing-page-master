d7839b0d1aa59b332a01a3d0de8f8f41543cd742
Wisdom Ochei <wisdomofgod2003@gmail.com>
2022-08-07 15:02:43 +0100

    Done with the development of mobile...No javascript interactivity yet

---
+++ app.js
@@ -0,0 +1,10 @@
+var x = document.querySelector(".drop-arrow");
+var y = document.querySelector(".answer");
+x.addEventListener("click", function(){
+    y.classList.add("answer1");
+    y.classList.remove("answerss");
+})
+
+
+
+
